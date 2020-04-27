b1683a08795c33910677be2d676dca1400ed0550
Teogenes <teogenes.santos@gmail.com>
2020-04-27 18:29:27 -0300

    FindById method implementation

--- src/main/java/com/teosantos/course/resources/UserResource.java
+++ src/main/java/com/teosantos/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll(){
 		List<User> listUser = userService.findAll();
 		return ResponseEntity.ok().body(listUser);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 
 }
--- src/main/java/com/teosantos/course/services/UserService.java
+++ src/main/java/com/teosantos/course/services/UserService.java
@@ -1,6 +1,7 @@
 package com.teosantos.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
 	}
 }
