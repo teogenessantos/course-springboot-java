2760b4a4aef52cc4dd14c612081390009f43b4ce
Teogenes <teogenes.santos@gmail.com>
2020-04-30 16:59:05 -0300

    Category

---
+++ src/main/java/com/teosantos/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.teosantos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teosantos.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/teosantos/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.teosantos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teosantos.course.entities.Category;
+import com.teosantos.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category.get();
+	}
+}
