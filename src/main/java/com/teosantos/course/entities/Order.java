7ed1277a94de8a3fa7e88ca261d00288ea02dfba
Teogenes <teogenes.santos@gmail.com>
2020-05-02 10:24:24 -0300

    Payment, one-to-one association

--- src/main/java/com/teosantos/course/entities/Order.java
+++ src/main/java/com/teosantos/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -31,6 +33,9 @@ public class Order implements Serializable{
 	private Instant moment;
 	
 	private Integer orderStatus;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -75,6 +80,14 @@ public class Order implements Serializable{
 		if(orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public User getClient() {
