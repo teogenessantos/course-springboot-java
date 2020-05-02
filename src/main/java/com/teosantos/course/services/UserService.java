b5af64ca663fdf4d865a8f689296aad5a3214daf
Teogenes <teogenes.santos@gmail.com>
2020-05-02 11:03:48 -0300

    User insert

--- src/main/java/com/teosantos/course/services/UserService.java
+++ src/main/java/com/teosantos/course/services/UserService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.teosantos.course.entities.User;
 import com.teosantos.course.repositories.UserRepository;
@@ -22,5 +25,9 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.get();
+	}
+	
+	public User insert(User user) {
+		return userRepository.save(user);
 	}
 }
